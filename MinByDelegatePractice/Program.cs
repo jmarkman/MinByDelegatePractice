d1b62aa7da24deee4c6cf451a104e17f3e04e555
Jonathan Markman <jmarkman446@gmail.com>
2018-06-07 00:39:56 -0400

    initial commit - requirements for challenge

--- MinByDelegatePractice/Program.cs
+++ MinByDelegatePractice/Program.cs
@@ -8,6 +8,18 @@ namespace MinByDelegatePractice
 {
     class Program
     {
+        /*
+         * Write MinBy, a function that does:
+         * var shortest = texts.MinBy(x => x.Length);
+         * 
+         * MinBy is retrieving the shortest length string in a list by accepting a lambda as a param
+         * 
+         * Test Case:
+         * Assert.AreEqual("h", new[] {"lol", "asdf", "h", "hello world"}.MinBy(x => x.Length));
+         * 
+         * Can ignore dupes and return the first, or return an IEnumerable<T>
+         * Do whatever on an empty sequence, prefer to throw an exception
+         */ 
         static void Main(string[] args)
         {
         }
